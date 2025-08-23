93309815bca522d89ccc69a9f8a6218145dbcc3c
ddiver02 <hyunjoongbox@gmail.com>
2025-08-24 01:22:41 +0900

    page:console.log delete

--- app/page.tsx
+++ app/page.tsx
@@ -22,10 +22,6 @@ export default function Home() {
     loadMessages();
   }, []);
 
-console.log("SUPABASE_URL", process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log("SUPABASE_KEY", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.slice(0,10) + "...");
-
-
   async function handleSubmit() {
     if (!text) return;
     const supabase = getSupabase();
